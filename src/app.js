c342ad3b7b0de51c6c4f673b1b80292bdaeda1b6
rajpratap29 <therajpratapofficial@gmail.com>
2026-02-25 11:56:40 +0530

    Implement logger and middlewares

--- src/app.js
+++ src/app.js
@@ -1,8 +1,30 @@
-import express from "express";
+import express, { urlencoded } from "express";
+import logger from "#config/logger.js";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(
+  morgan("combined", {
+    stream: {
+      write: message => {
+        logger.info(message.trim());
+      },
+    },
+  })
+);
 
 app.get("/", (req, res) => {
+  logger.info("Hello from Acquisitions!");
+
   res.status(200).send("Hello from Acquisitions!");
 });
 
